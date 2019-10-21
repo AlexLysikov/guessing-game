b69a7ecd47bc5f0a116a113ead2c4a97386f5d0d
AlexLysikov <alex.lysikov.82@gmail.com>
2019-10-21 20:32:35 +0300

    feat: task complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.segmentMin = 0;
+        this.segmentMax = 0;
+    }
+    
     setRange(min, max) {
-
+        this.segmentMin = min;
+        this.segmentMax = max;
     }
 
     guess() {
-
+        return Math.round ((this.segmentMax - this.segmentMin) / 2 + this.segmentMin);
     }
 
     lower() {
-
+        this.segmentMax = this.guess();
     }
 
     greater() {
-
+        this.segmentMin = this.guess();
     }
 }
 
